d69022f5b132d3c5c3b39ad31154151d5f4ae0f6
suyeon <122500193+bagoye@users.noreply.github.com>
2023-03-04 23:15:36 +0900

    [Silver V] Title: 올림픽, Time: 184 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+arr=[]
+for i in range(N):
+    arr.append(list(map(int,input().split())))
+    arr=sorted(arr, key=lambda x: (-x[1], -x[2], -x[3]))
+
+for i in range(N):
+    if arr[i][0] == K:
+
+        nation = i
+
+for i in range(N):
+    if arr[nation][1:] == arr[i][1:]:
+        print(i+1)
+        break
