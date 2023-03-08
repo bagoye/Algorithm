91faf15e70f943db6f0cd5e59d2ed61f8f7e82ba
suyeon <122500193+bagoye@users.noreply.github.com>
2023-03-09 00:27:46 +0900

    [Silver V] Title: 올림픽, Time: 100 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,15 +1,17 @@
 N, K = map(int, input().split())
-arr=[]
-for i in range(N):
-    arr.append(list(map(int,input().split())))
-    arr=sorted(arr, key=lambda x: (-x[1], -x[2], -x[3]))
+lst = [list(map(int, input().split())) for _ in range(N)]   # 국가, 금, 은, 동
+score = [[] for _ in range(N + 1)]    # 각 국가의 메달 수를 담을 리스트 생성
 
-for i in range(N):
-    if arr[i][0] == K:
+for i in range(N):          # 국가번호를 score 리스트의 인덱스로 사용하고 메달 수를 담을거임
+    score[lst[i][0]] = lst[i][1:4]
 
-        nation = i
+cnt = 0
+for i in range(1, N+1):
+    if score[i][0] > score[K][0]:   # 다른 국가 금메달 수가 K보다 많으면 1 증가
+        cnt += 1
+    if score[i][0] == score[K][0] and score[i][1] > score[K][1]:   # 금메달 수 같고 은메달 수 비교
+        cnt += 1
+    if score[i][0] == score[K][0] and score[i][1] == score[K][1] and score[i][2] > score[K][2]:   # 금, 은 같고 동메달 비교
+        cnt += 1
 
-for i in range(N):
-    if arr[nation][1:] == arr[i][1:]:
-        print(i+1)
-        break
+print(cnt + 1)
