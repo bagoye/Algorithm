6c338f7fc895a140c858e634d2112d7b1eb1978a
suyeon <122500193+bagoye@users.noreply.github.com>
2023-03-15 17:52:38 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,32 +1,22 @@
-N = int(input())        # 스위치 개수
-s_lst = [-1] + list(map(int, input().split()))
-S = int(input())
-for _ in range(S):
-    gender, s_num = map(int, input().split())
-    if gender == 1:    # 남자인 경우
-        for i in range(s_num, N+1, s_num):
-            if s_lst[i] == 0:
-                s_lst[i] = 1
-            else:
-                s_lst[i] = 0
-    else:              # 여자면
-        if s_lst[s_num] == 0:
-            s_lst[s_num] = 1
-        else:
-            s_lst[s_num] = 0
-        for i in range(1, N//2):
-            if s_num - i < 1 or s_num + i > N: break
-            if s_lst[s_num-i] == s_lst[s_num+i]:
-                if s_lst[s_num-i] == 0:
-                    s_lst[s_num-i] = 1
-                    s_lst[s_num+i] = 1
-                else:
-                    s_lst[s_num-i] = 0
-                    s_lst[s_num+i] = 0
-            else:
-                break
+N = int(input())                        # 방의 갯수
+lst = list(map(int,input().split()))    # 각 방들의 상태
 
-for i in range(1, N+1):
-    print(s_lst[i], end=' ')
-    if i % 20 == 0:
-        print()
+for i in range(int(input())):       # 인원 수
+    a,b = map(int,input().split())  # 성별 과 방을 받아줌
+
+    if a == 1:                      # 남자
+        for i in range(b-1, N, b):      # b의 배수
+            if i > N: break             # 범위 벗어날 경우
+            lst[i] = 1 - lst[i]
+    else:                           # 여자
+        y = b - 1
+        lst[y] = 1 - lst[y]
+        for num in range(1,N//2):
+            if y < num or y + num > N-1: break      # 범위 벗어날 경우
+            if lst[y - num] != lst[y + num]: break  # 양쪽 방이 다를 경우
+            if lst[y - num] == lst[y + num]:
+                lst[y-num] = 1 - lst[y-num]     # 좌측방
+                lst[y+num] = 1 - lst[y+num]     # 우측방
+
+for i in range(0, len(lst), 20):
+    print(*lst[i:i + 20])
