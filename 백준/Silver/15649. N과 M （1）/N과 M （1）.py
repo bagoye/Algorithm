52efea67af86725bff7c8e1d6c77e38157a9dae1
suyeon <122500193+bagoye@users.noreply.github.com>
2024-01-04 10:07:16 +0900

    [Silver III] Title: N과 M (1), Time: 200 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,15 @@
-n, m = list(map(int, input().split()))
-s = []
+n, m = map(int,input().split())
+ans = []
 
-def dfs():
-    if len(s) == m:
-        print(' '.join(map(str, s)))
+def backtracking():
+    if len(ans) == m: # 길이가 m인 조합을 찾은 경우 출력하고 함수 끝
+        print(*ans)
         return
 
-    for i in range(1, n + 1):
-        if i not in s:
-            s.append(i)
-            dfs()
-            s.pop()
+    for i in range(1, n+1): # 아니면 모든 숫자 반복
+        if i not in ans:
+            ans.append(i)
+            backtracking() # 재귀
+            ans.pop()
 
-dfs()
+backtracking()
