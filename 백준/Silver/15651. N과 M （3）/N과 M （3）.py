1c2f9fc1e824456a864193fc02da1692156e8ff2
suyeon <122500193+bagoye@users.noreply.github.com>
2024-01-06 11:11:48 +0900

    [Silver III] Title: N과 M (3), Time: 2024 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,15 +1,16 @@
 n, m = map(int, input().split())
-s = []
-
+ans = []
 
 def dfs():
-    if len(s) == m:
-        print(' '.join(map(str, s)))
+    # 현재까지 찾은 순열의 길이가 m과 같으면 순열을 출력
+    if len(ans) == m:
+        print(*ans)
         return
 
+    # 순열에 추가할 숫자를 결정하는 반복문
     for i in range(1, n + 1):
-        s.append(i)
+        ans.append(i)
         dfs()
-        s.pop()
+        ans.pop()
 
 dfs()
