7e5fd63bb03bd1f3ee0fbfd7cb201b0dd0969bbe
suyeon <122500193+bagoye@users.noreply.github.com>
2024-01-22 09:38:37 +0900

    [Silver V] Title: 국회의원 선거, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,21 +1,17 @@
-import sys, heapq
-input = sys.stdin.readline
-
-n = int(input())
-win = int(input())
-nums = []
-
+n = int(input()) # 후보의 수
+dasom = int(input()) # 기호 1번을 찍으려고 하는 사람의 수
+vote = [] # 다른 후보를 찍으려고 하는 사람의 수
+count = 0 # 매수해야하는 사람의 수
 for _ in range(n - 1):
-    num = int(input())
-    heapq.heappush(nums, (-num, num))
-
-cnt = 0
-while nums:
-    num = heapq.heappop(nums)[1]
-    if num >= win:
-        num -= 1
-        win += 1
-        cnt += 1
-        heapq.heappush(nums, (-num, num))
-
-print(cnt)
+  vote.append(int(input()))
+vote.sort(reverse=True) # 내림차순 정렬
+if n == 1:
+  print(0)
+else:
+  # 표가 제일 많은 후보를 찍으려고 하는사람을 매수
+  while vote[0] >= dasom:
+    dasom += 1
+    vote[0] -= 1
+    count += 1
+    vote.sort(reverse=True)
+  print(count)
