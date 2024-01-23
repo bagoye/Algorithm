84483c119d65ff2c055cf80cf0429a5911419f1e
suyeon <122500193+bagoye@users.noreply.github.com>
2024-01-23 12:03:28 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,22 +1,28 @@
-N = int(input())                        # 방의 갯수
-lst = list(map(int,input().split()))    # 각 방들의 상태
+import sys
+input = sys.stdin.readline
 
-for i in range(int(input())):       # 인원 수
-    a,b = map(int,input().split())  # 성별 과 방을 받아줌
+n = int(input())
+switch = [0] + list(map(int, input().split()))
 
-    if a == 1:                      # 남자
-        for i in range(b-1, N, b):      # b의 배수
-            if i > N: break             # 범위 벗어날 경우
-            lst[i] = 1 - lst[i]
-    else:                           # 여자
-        y = b - 1
-        lst[y] = 1 - lst[y]
-        for num in range(1,N//2):
-            if y < num or y + num > N-1: break      # 범위 벗어날 경우
-            if lst[y - num] != lst[y + num]: break  # 양쪽 방이 다를 경우
-            if lst[y - num] == lst[y + num]:
-                lst[y-num] = 1 - lst[y-num]     # 좌측방
-                lst[y+num] = 1 - lst[y+num]     # 우측방
+def change(x):
+    global switch
+    switch[x] = abs(switch[x]-1)
 
-for i in range(0, len(lst), 20):
-    print(*lst[i:i + 20])
+for _ in range(int(input())):
+    gender, num = map(int, input().split())
+    i = 1
+    if gender == 1:
+        while num * i <= n:
+            change(num * i)
+            i += 1
+    elif gender == 2:
+        change(num)
+        while 1 <= num-i and num+i <= n and switch[num-i] == switch[num+i]:
+            change(num-i)
+            change(num+i)
+            i += 1
+
+for i in range(1, n+1):
+    print(switch[i], end=" ")
+    if not i % 20:
+        print()
