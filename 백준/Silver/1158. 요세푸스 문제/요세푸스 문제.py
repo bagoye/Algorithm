296731437f8f0b3f327a31d7678ccb6d9e2eb63a
suyeon <122500193+bagoye@users.noreply.github.com>
2024-01-24 12:25:14 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int,input().split())
+arr = [i for i in range(1, n+1)]
+
+ans = []
+num = 0
+
+for t in range(n):
+    num += k-1
+    if num >= len(arr):  
+        num = num % len(arr)
+ 
+    ans.append(str(arr.pop(num)))
+    
+print("<",", ".join(ans)[:],">", sep='')
