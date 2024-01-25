25b031dfa6e876a9ecfa8506c4575ba13ae1ac3f
suyeon <122500193+bagoye@users.noreply.github.com>
2024-01-25 15:35:11 +0900

    [Silver V] Title: 덩치, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+arr = []
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    arr.append((x, y))
+
+for i in arr:
+    rank = 1
+    for j in arr:
+        if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+    print(rank, end = " ")
