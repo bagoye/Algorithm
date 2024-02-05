b9ae3f929258c93d5e9cba2896f90305cef3cfa1
suyeon <122500193+bagoye@users.noreply.github.com>
2024-02-05 13:20:37 +0900

    [Silver IV] Title: 베스트셀러, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,22 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
 sell = {}
 
-for i in range(n):
-    name = input()
-    if name not in sell:
-        sell[name] = 1
+for i in range(int(input())):
+    book = input()
+    if book in sell:
+        sell[book] += 1
     else:
-        sell[name] += 1
+        sell[book] = 1
         
 max_value = max(sell.values())
 
-best = []
-for key, value in sell.items():
-    if value == max_value:
-        best.append(key)
+best = sorted([key for key, value in sell.items() if value == max(sell.values())])
 
-best = sorted(best)
 print(best[0])
