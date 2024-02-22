3e3e2a04726fc753d25b66fe5ab5efd3a83a0d7e
suyeon <122500193+bagoye@users.noreply.github.com>
2024-02-22 15:28:24 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,15 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-def fib(n):
-    zeros=[1,0,1]
-    ones=[0,1,1]
-    if n >= 3:
-        for i in range(2,n):
-            zeros.append(zeros[i-1] + zeros[i])
-            ones.append(ones[i-1] + ones[i])
-    print(f"{zeros[n]} {ones[n]}")
- 
 for _ in range(int(input())):
     n = int(input())
-    fib(n)
+    zero, one = 1, 0
+    for i in range(n):
+        zero ,one = one, zero + one
+    print(zero, one)
