701af8d6093bfd523e0ed237d9eca1b49e9d2fd4
suyeon <122500193+bagoye@users.noreply.github.com>
2024-02-28 14:48:27 +0900

    [Silver III] Title: N과 M (1), Time: 196 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,18 @@
-n, m = map(int,input().split())
-ans = []
+import sys
+input = sys.stdin.readline
 
-def backtracking():
-    if len(ans) == m: # 길이가 m인 조합을 찾은 경우 출력하고 함수 끝
-        print(*ans)
-        return
+def bfs():
+  if len(result) == m:
+    print(*result)
+    return
+  
+  for i in range(1, n+1):
+    if i not in result:
+      result.append(i)
+      bfs()
+      result.pop()
 
-    for i in range(1, n+1): # 아니면 모든 숫자 반복
-        if i not in ans:
-            ans.append(i)
-            backtracking() # 재귀
-            ans.pop()
+n, m = map(int, input().split())
+result = []
 
-backtracking()
+bfs()
