2f3843c9f583e7f9a72532cdffe0aef61b702bd4
suyeon <122500193+bagoye@users.noreply.github.com>
2024-03-11 11:32:38 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,11 @@
+function solution(brown, yellow) {
+    var answer = [];
+    for (let i = 3; i <= (brown + yellow) / i; i++) {
+        let x = Math.floor((brown + yellow) / i);
+        if ((x-2)*(i-2) === yellow) {
+            answer.push(x, i);
+            break;
+        }
+    }
+    return answer;
+}
