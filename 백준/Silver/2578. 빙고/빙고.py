9640e1d1c1fb7ec02311707c6f6f6ee56f2e6d22
suyeon <122500193+bagoye@users.noreply.github.com>
2024-03-19 09:44:28 +0900

    [Silver IV] Title: 빙고, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,38 +1,51 @@
-arr = [list(map(int, input().split())) for _ in range(5)]
-bingo = [list(map(int, input().split())) for _ in range(5)]
+import sys
+input = sys.stdin.readline
 
-def binggo():
-    cnt = 0
-    for k in range(5):
-        for l in range(5):
-            for i in range(5):
-                for j in range(5):
-                    if arr[i][j] == bingo[k][l]:
-                        arr[i][j] = 0
-                        cnt += 1
-                    if cnt >= 12:
-                        rlt = 0
-                        for r in range(5):
-                            if sum(arr[r]) == 0:
-                                rlt += 1
-                        for y in range(5):
-                            count = 0
-                            for x in range(5):
-                                if arr[x][y] == 0:
-                                    count += 1
-                            if count == 5:
-                                rlt += 1
-                        cross1 = []
-                        cross2 = []
-                        for a in range(5):
-                            cross1.append(arr[a][a])
-                            cross2.append(arr[a][4-a])
-                        if sum(cross1) == 0:
-                            rlt += 1
-                        if sum(cross2) == 0:
-                            rlt += 1
+board1 = [list(map(int, input().split())) for _ in range(5)]
+board2 = [list(map(int, input().split())) for _ in range(5)]
 
-                        if rlt >= 3:
-                            return cnt
+cnt = 0
 
-print(binggo())
+# 빙고 개수 세기
+def solution(array: list):
+    bingo_number = 0
+    # 가로가 빙고일때
+    for i in range(5):
+        if sum(array[i]) == 0:
+            bingo_number += 1
+
+    # 세로가 빙고일때
+    for i in range(5):
+        count_temp = 0
+        for j in range(5):
+            if array[j][i] == 0:
+                count_temp += 1
+        if count_temp == 5:
+            bingo_number += 1
+
+    # 대각선이 빙고일 때
+    temp_up = []  # 우하단 빙고
+    temp_down = []  # 우상단 빙고
+    for i in range(5):
+        temp_up.append(array[i][i])
+        temp_down.append(array[i][4 - i])
+
+    if sum(temp_up) == 0:
+        bingo_number += 1
+    if sum(temp_down) == 0:
+        bingo_number += 1
+
+    return bingo_number
+
+
+for i in range(5):
+    for j in range(5):
+        for k in range(5):
+            for h in range(5):  # 사회자가 부른 수를 비교하기위해 4번의 반복문을 돈다.
+                if board2[i][j] == board1[k][h]:  # 만약 해당 번호를 찾았다면
+                    board1[k][h] = 0  # 그 번호를 0으로 만들고
+                    cnt += 1  # 부른횟수를 +1 해준다.
+                if cnt >= 12:  # 만약 cnt 값이 12이상이라면(3빙고가 나오기 위한 최소한의 조건)
+                    if solution(board1) >= 3:  # 빙고개수를 세어 3이상이라면
+                        print(cnt)  # 그 횟수를 출력하고
+                        exit()  # 코드종료한다.
