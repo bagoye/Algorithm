85ad27947d7020c4e377eb0e6de64cc32d25a066
suyeon <122500193+bagoye@users.noreply.github.com>
2024-03-29 11:46:01 +0900

    [Gold V] Title: AC, Time: 212 ms, Memory: 45272 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+for i in range(int(input())):
+    p = input()
+    n = int(input())
+    arr = input()[1:-1].split(',')
+    q = deque(arr)
+    flag = 0
+
+    if n == 0:
+        q = []
+
+    for j in p:
+        if j == 'R':
+            flag += 1
+        elif j == 'D':
+            if len(q) == 0:
+                print("error")
+                break
+            else:
+                if flag % 2 == 0:
+                    q.popleft()
+                else:
+                    q.pop()
+
+    else:
+        if flag % 2 == 0:
+            print("[" + ",".join(q) + "]")
+        else:
+            q.reverse()
+            print("[" + ",".join(q) + "]")
