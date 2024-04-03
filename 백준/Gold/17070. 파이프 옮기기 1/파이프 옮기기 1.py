a9b737c73d76493be7606bbf206bd0fa0d1cd125
suyeon <122500193+bagoye@users.noreply.github.com>
2024-04-03 15:16:55 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,49 +1,23 @@
-import sys
-count = 0
-N = int(input())
-home = []
+def solution():
+    dp[0][0][1] = 1
+    for i in range(2, n):
+        if board[0][i] == 0:
+            dp[0][0][i] = dp[0][0][i - 1]
 
-for _ in range(N):
-    home.append([int(x) for x in sys.stdin.readline().rstrip().split()])
+    for r in range(1, n):
+        for c in range(1, n):
+            if board[r][c] == 0 and board[r][c - 1] == 0 and board[r - 1][c] == 0:
+                dp[1][r][c] = dp[0][r - 1][c - 1] + dp[1][r - 1][c - 1] + dp[2][r - 1][c - 1]
 
-def dfs(x,y,state): # state 0: 가로, 1: 세로 , 2: 대각선
-    global count
-    if x == N - 1 and y == N - 1:
-        count += 1
-        return
+            if board[r][c] == 0:
+                dp[0][r][c] = dp[0][r][c - 1] + dp[1][r][c - 1]
+                dp[2][r][c] = dp[2][r - 1][c] + dp[1][r - 1][c]
 
-    if state == 0:
-        if y == N - 1: # 이동불가
-            return
+    # 최종 결과 출력
+    print(sum(dp[i][n - 1][n - 1] for i in range(3)))
 
-        if 0 <= x < N and 0 <= y + 1 < N and home[x][y + 1] == 0:
-            dfs(x, y + 1, 0)
 
-        if 0 <= x + 1 < N and 0 <= y + 1 < N and home[x][y + 1] == 0 and home[x + 1][y] == 0 and home[x + 1][
-            y + 1] == 0:
-            dfs(x + 1, y + 1, 2)
-
-    elif state == 1:
-        if x == N - 1: # 이동불가
-            return
-
-        if 0 <= x + 1 < N and 0 <= y < N and home[x + 1][y] == 0:
-            dfs(x + 1, y, 1)
-
-        if 0 <= x + 1 < N and 0 <= y + 1 < N and home[x][y + 1] == 0 and home[x + 1][y] == 0 and home[x + 1][
-            y + 1] == 0:
-            dfs(x + 1, y + 1, 2)
-
-    elif state == 2:
-        if 0 <= x < N and 0 <= y + 1 < N and home[x][y + 1] == 0:
-            dfs(x, y + 1, 0)
-
-        if 0 <= x + 1 < N and 0 <= y < N and home[x + 1][y] == 0:
-            dfs(x + 1, y, 1)
-
-        if 0 <= x + 1 < N and 0 <= y + 1 < N and home[x][y + 1] == 0 and home[x + 1][y] == 0 and home[x + 1][
-            y + 1] == 0:
-            dfs(x + 1, y + 1, 2)
-
-dfs(0,1,0)
-print(count)
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(3)]
+solution()
