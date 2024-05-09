c483ea3b1ebb0e843778ba2afa21f4f2b24ef481
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-09 22:36:27 +0900

    [Bronze I] Title: 더하기 사이클, Time: 112 ms, Memory: 12852 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,23 +1,24 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.Scanner;
  
 public class Main {
  
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
  
-		int N = Integer.parseInt(br.readLine());
-		
+		int N = in.nextInt();
+		in.close();
+        
 		int cnt = 0;
 		int copy = N;
         
-		do {
+		while (true) {
 			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
 			cnt++;
-		} while (copy != N);
-		
+ 
+			if (copy == N) {
+				break;
+			}
+		}
 		System.out.println(cnt);
 	}
 }
- 
