845926ac6249ff8a2dde4ac508df4eba00cad4cc
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-23 13:18:46 +0900

    [Bronze II] Title: 수 정렬하기, Time: 96 ms, Memory: 12396 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,30 +1,30 @@
 import java.io.*;
 
 public class Main {
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(arr[i] > arr[j]) {
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+        // 버블정렬 구현
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
         }
 
-        for(int i : arr) {
-            System.out.println(i);
+        for (int i = 0; i < n; i++) {
+            bw.write(a[i] + "\n");
         }
+        bw.close();
     }
-
 }
