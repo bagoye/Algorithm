e540fea484a1409bff6233babc7f3c0deda74e6d
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-24 11:03:37 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,22 +1,22 @@
 def solution(progresses, speeds):
     answer = []
     idx = 0
-
+    
     while True:
         cnt = 0
-
+        
         for i in range(len(progresses)):
             progresses[i] += speeds[i]
-
+        
         for i in range(idx, len(progresses)):
             if progresses[i] >= 100:
                 cnt += 1
             else:
                 break
-
+        
         if cnt > 0:
             answer.append(cnt)
             idx += cnt
-
-        if  len(progresses) == sum(answer):
+        
+        if len(progresses) == sum(answer):
             return answer
