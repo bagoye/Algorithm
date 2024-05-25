ba0bd2f1429c16548f6f377cc4019d0d439d9219
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-25 22:42:30 +0900

    [Bronze IV] Title: 주사위 세개, Time: 80 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if (a != b && b != c && a != c) {
+            int max = Math.max(a, Math.max(b, c));
+            System.out.println(max * 100);
+        } else if (a == b && a == c) {
+            System.out.println(10000 + a * 1000);
+        } else if(a == b || a == c) {
+            System.out.println(1000 + a * 100);
+        } else {
+            System.out.println(1000 + b * 100);
+        }
+
+    }
+}
