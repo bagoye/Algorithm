040cce244e73b61ab7aaf98f3514a72148a0c5a7
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-25 22:34:49 +0900

    [Bronze V] Title: 사분면 고르기, Time: 76 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        if (x > 0 && y > 0) {
+            bw.write("1");
+        } else if (x < 0 && y > 0) {
+            bw.write("2");
+        } else if (x < 0 && y < 0) {
+            bw.write("3");
+        } else {
+            bw.write("4");
+        }
+
+        bw.close();
+    }
+}
