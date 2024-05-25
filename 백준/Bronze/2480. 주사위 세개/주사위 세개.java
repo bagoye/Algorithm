ba6527bcfe37491951eed4ef8ce1bf1310ea92d6
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-25 22:43:40 +0900

    [Bronze IV] Title: 주사위 세개, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -15,14 +15,14 @@ public class Main {
 
         if (a != b && b != c && a != c) {
             int max = Math.max(a, Math.max(b, c));
-            System.out.println(max * 100);
+            bw.write(max * 100 + "\n");
         } else if (a == b && a == c) {
-            System.out.println(10000 + a * 1000);
+            bw.write(10000 + a * 1000 + "\n");
         } else if(a == b || a == c) {
-            System.out.println(1000 + a * 100);
+            bw.write(1000 + a * 100 + "\n");
         } else {
-            System.out.println(1000 + b * 100);
+            bw.write(1000 + b * 100 + "\n");
         }
-
+        bw.close();
     }
 }
