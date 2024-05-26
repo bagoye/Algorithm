80f7cc6a079414902efc8064d71c644f68544df8
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-26 16:52:34 +0900

    [Bronze II] Title: 상수, Time: 80 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        bw.write((a > b ? a : b) + "\n");
+        bw.close();
+        
+    }
+}
