c0477be5fd14f704120daed87e0883da217624c2
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-26 21:46:39 +0900

    [Bronze III] Title: 최댓값, Time: 84 ms, Memory: 11656 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int[][] arr = new int[9][9];
+        int max = 0;
+        int x = 1, y = 1;
+
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    x = i + 1;
+                    y = j + 1;
+                }
+            }
+        }
+        bw.write(max + "\n");
+        bw.write(x + " " + y);
+        bw.close();
+
+    }
+}
