ad7a9ec57312624c88965375d30669bd77254627
suyeon <122500193+bagoye@users.noreply.github.com>
2024-05-31 16:56:07 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 11692 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int answer = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] > m) {
+                        continue;
+                    } else {
+                        answer = Math.max(answer, arr[i] + arr[j] + arr[k]);
+                    }
+                }
+            }
+        }
+
+        bw.write(answer + "");
+        bw.close();
+    }
+}
