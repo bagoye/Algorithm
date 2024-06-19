c4cb9fd5dfe1867a29dbfb33468a5ce29c7e91d8
suyeon <122500193+bagoye@users.noreply.github.com>
2024-06-19 17:10:11 +0900

    [Silver III] Title: 모든 순열, Time: 176 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+def dfs():
+    if len(arr) == n:
+        print(*arr)
+        return
+    for i in range(1, n+1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+dfs()
