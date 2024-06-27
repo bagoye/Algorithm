949239eedecf43d74efc0c976b8dd56914d237ca
suyeon <122500193+bagoye@users.noreply.github.com>
2024-06-27 14:42:06 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,15 +1,13 @@
 import sys
-input = sys.stdin.readline
 
+input = sys.stdin.readline
 n = int(input())
-# dp 테이블 2차원으로 실계, 초기화
-dp = [[0] * 10 for _ in range(n+1)]
-dp[1] = [1] * 10
+
+dp = [1] * 10
 
 for i in range(2, n+1):
     for j in range(10):
-        dp[i][j] = sum(dp[i-1][j:])
+        dp[j] = sum(dp[j:])
 
-answer = sum(dp[n]) % 10007
-
+answer = sum(dp) % 10007
 print(answer)
