d8e22413de8a6d3ee41a2449bb23e4532632f421
suyeon <122500193+bagoye@users.noreply.github.com>
2024-06-28 16:16:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 440 ms, Memory: 118900 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,18 +1,23 @@
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
+
     static int n, m, u, v;
-    static boolean[] visit = new boolean[1001];
-    static int[][] arr = new int[1001][1001];
+    static int[][] arr;
+    static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1][n + 1];
+        visit = new boolean[n + 1];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -20,27 +25,29 @@ public class Main {
             v = Integer.parseInt(st.nextToken());
             arr[u][v] = arr[v][u] = 1;
         }
-        int answer = 0;
 
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
             if (!visit[i]) {
-                dfs(i);
+                bfs(i);
                 answer++;
             }
         }
 
-        bw.write(answer + "\n");
+        bw.write(answer + "");
         bw.close();
     }
 
-    public static void dfs(int idx) {
-        if (visit[idx]) {
-            return;
-        } else {
-            visit[idx] = true;
+    public static void bfs(int idx) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(idx);
+        visit[idx] = true;
+        while (!q.isEmpty()) {
+            int temp = q.poll();
             for (int i = 1; i <= n; i++) {
-                if (arr[idx][i] == 1) {
-                    dfs(i);
+                if (!visit[i] && arr[temp][i] == 1) {
+                    visit[i] = true;
+                    q.offer(i);
                 }
             }
         }
