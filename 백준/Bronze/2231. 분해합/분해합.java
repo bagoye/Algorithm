904f5baa4e00ef2c0634bc316e25b99c4334653b
suyeon <122500193+bagoye@users.noreply.github.com>
2024-06-29 22:12:11 +0900

    [Bronze II] Title: 분해합, Time: 92 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = i;
+            int sum = 0;
+
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if (sum + i == n) {
+                answer = i;
+                break;
+            }
+        }
+        bw.write(answer + "");
+        bw.close();
+
+    }
+}
