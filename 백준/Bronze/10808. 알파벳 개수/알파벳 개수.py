8c793c224d470555df755498452a7686d1fdd6a8
suyeon <dltd0912@gmail.com>
2024-08-28 11:01:18 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+s = list(input())
+lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in range(len(lst)):
+    ans = s.count(lst[i])
+    print(ans, end=" ")
