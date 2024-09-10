b9f76ef226d89c8a7e0e52ac7f48acff06506245
suyeon <dltd0912@gmail.com>
2024-09-10 20:35:08 +0900

    [Silver II] Title: 예산, Time: 108 ms, Memory: 13464 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -9,17 +9,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        int s = 0;
-        int e = -1;
+
+        int left = 0;
+        int right = -1;
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            e = Math.max(e, arr[i]);
+            right = Math.max(right, arr[i]);
         }
+
         int m = Integer.parseInt(br.readLine());
 
-        while (s <= e) {
-            int mid = (s + e) / 2;
+        while (left <= right) {
+            int mid = (left + right) / 2;
             int sum = 0;
             for (int i = 0; i < n; i++) {
                 if (arr[i] > mid) {
@@ -30,12 +33,12 @@ public class Main {
             }
 
             if (sum <= m) {
-                s = mid + 1;
+                left = mid + 1;
             } else {
-                e = mid - 1;
+                right = mid - 1;
             }
         }
-        bw.write(e + "");
+        bw.write(right + "");
         bw.close();
     }
 }
