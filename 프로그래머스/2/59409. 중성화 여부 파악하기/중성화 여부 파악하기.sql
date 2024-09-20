026803074244e2c1d8b7b6b857670c30f1560035
suyeon <dltd0912@gmail.com>
2024-09-20 10:47:00 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,4 +1,9 @@
 SELECT ANIMAL_ID, NAME,
-IF (SEX_UPON_INTAKE LIKE 'Neutered%' OR SEX_UPON_INTAKE LIKE 'Spayed%', 'O', 'X') AS 중성화
+    CASE WHEN 
+        (SEX_UPON_INTAKE LIKE '%Neutered%') OR 
+        (SEX_UPON_INTAKE LIKE '%Spayed%') 
+    THEN 'O' 
+    ELSE 'X' 
+    END AS '중성화'
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
