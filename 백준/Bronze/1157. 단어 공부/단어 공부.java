b64f7b74f5c50499b089f9f22defb4108c13adcc
suyeon <dltd0912@gmail.com>
2024-09-26 14:58:23 +0900

    [Bronze I] Title: 단어 공부, Time: 176 ms, Memory: 24524 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -4,22 +4,25 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static int[] getAlphabetCnt (String str) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            cnt[str.charAt(i) - 'A']++;
+        }
+        return cnt;
+    }
+
     public static void main(String[] args) throws IOException {
         String str = br.readLine().toUpperCase();
-        int[] arr = new int[26];
-
-        for (int i = 0; i < str.length(); i++) {
-            int num = str.charAt(i) - 'A';
-            arr[num]++;
-        }
+        int[] cnt = getAlphabetCnt(str);
 
         int max = -1;
         char answer = '?';
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                answer = (char) (i + 65);
-            } else if (arr[i] == max) answer = '?';
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] > max) {
+                max = cnt[i];
+                answer = (char) ('A' + i);
+            } else if (cnt[i] == max) answer = '?';
         }
 
         bw.write(answer);
