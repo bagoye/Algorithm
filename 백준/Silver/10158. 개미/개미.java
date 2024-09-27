2ee6129161a50b57247c9a9dc044647607ceebbe
suyeon <dltd0912@gmail.com>
2024-09-27 14:01:23 +0900

    [Silver III] Title: 개미, Time: 64 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -15,21 +15,26 @@ public class Main {
         int q = Integer.parseInt(st.nextToken());
         int t = Integer.parseInt(br.readLine());
 
-        int timeX = t % (2 * w);
-        int dx = 1;
-        while (timeX-- > 0) {
-            if (p == w) dx = -1;
-            else if (p == 0) dx = 1;
-            p += dx;
-        }
+//        int timeX = t % (2 * w);
+//        int dx = 1;
+//        while (timeX-- > 0) {
+//            if (p == w) dx = -1;
+//            else if (p == 0) dx = 1;
+//            p += dx;
+//        }
+//
+//        int timeY = t % (2 * h);
+//        int dy = 1;
+//        while (timeY-- > 0) {
+//            if (q == h) dy = -1;
+//            else if (q == 0) dy = 1;
+//            q += dy;
+//        }
+        p = (t + p) % (2 * w);
+        q = (t + q) % (2 * h);
+        if (p > w) p = 2 * w - p;
+        if (q > h) q = 2 * h - q;
 
-        int timeY = t % (2 * h);
-        int dy = 1;
-        while (timeY-- > 0) {
-            if (q == h) dy = -1;
-            else if (q == 0) dy = 1;
-            q += dy;
-        }
         bw.write(p + " " + q);
         bw.close();
     }
