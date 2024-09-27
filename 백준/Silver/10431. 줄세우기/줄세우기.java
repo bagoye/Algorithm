42a147b2d5f8ea79c42ea93ff771cb223a5bd96d
suyeon <dltd0912@gmail.com>
2024-09-27 16:26:48 +0900

    [Silver V] Title: 줄세우기, Time: 116 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -10,21 +10,18 @@ public class Main {
         int p = Integer.parseInt(br.readLine());
         int[] arr = new int[20];
 
-        for (int i = 1; i < p + 1; i++) {
+        while (p-- > 0){
             int cnt = 0;
             st = new StringTokenizer(br.readLine());
             int t = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < 20; j++) {
-                arr[j] = Integer.parseInt(st.nextToken());
-            }
+            for (int j = 0; j < 20; j++) arr[j] = Integer.parseInt(st.nextToken());
 
             for (int j = 0; j < 20; j++) {
                 for (int k = 0; k < j; k++) {
-                    if (arr[j] < arr[k]) {
-                        cnt++;
-                    }
+                    if (arr[j] < arr[k]) cnt++;
                 }
             }
+
             bw.write(t + " " + cnt + "\n");
         }
         bw.close();
