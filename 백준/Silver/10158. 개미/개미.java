9adc1c305ae132a6bd71ab580a37d53093a37edd
suyeon <dltd0912@gmail.com>
2024-09-27 13:57:41 +0900

    [Silver III] Title: 개미, Time: 64 ms, Memory: 11696 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
+
+        int timeX = t % (2 * w);
+        int dx = 1;
+        while (timeX-- > 0) {
+            if (p == w) dx = -1;
+            else if (p == 0) dx = 1;
+            p += dx;
+        }
+
+        int timeY = t % (2 * h);
+        int dy = 1;
+        while (timeY-- > 0) {
+            if (q == h) dy = -1;
+            else if (q == 0) dy = 1;
+            q += dy;
+        }
+        bw.write(p + " " + q);
+        bw.close();
+    }
+}
