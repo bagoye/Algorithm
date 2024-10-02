509eb374447dfa898b8e7ffd936589f147e57c3b
suyeon <dltd0912@gmail.com>
2024-10-02 22:17:17 +0900

    [Silver II] Title: 좌표 압축, Time: 2120 ms, Memory: 364876 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -11,27 +9,19 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
+        int[] arr = new int[n];
+        Set<Integer> set = new TreeSet<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = i;
+            arr[i] = Integer.parseInt(st.nextToken());
+            set.add(arr[i]);
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-        int[] answer = new int[n];
+        Map<Integer, Integer> map = new HashMap<>();
         int idx = 0;
-        answer[arr[0][1]] = idx;
-        for (int i = 1; i < n; i++) {
-            if (arr[i][0] != arr[i-1][0]) idx++;
-            answer[arr[i][1]] = idx;
-        }
-        for (int x : answer) bw.write(x + " ");
+        for (int x : set) map.put(x, idx++);
+
+        for (int i = 0; i < n; i++) bw.write(map.get(arr[i]) + " ");
         bw.close();
     }
 }
