3de66ae5fc34dbc4b55300f872f89d607326f542
suyeon <dltd0912@gmail.com>
2024-10-02 13:00:17 +0900

    [Silver V] Title: 단어 정렬, Time: 260 ms, Memory: 32596 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 public class Main {
 
@@ -15,9 +13,6 @@ public class Main {
 
         for (int i = 0; i < n; i++) arr[i] = br.readLine();
 
-        Set<String> set = new LinkedHashSet<>(Arrays.asList(arr));
-        arr = set.toArray(new String[0]);
-
         Arrays.sort(arr, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
@@ -27,9 +22,10 @@ public class Main {
                 return o1.length() - o2.length();
             }
         });
-
-        for (String s : arr) bw.write(s + "\n");
-
+        bw.write(arr[0] + "\n");
+        for (int i = 1; i < n; i++) {
+            if (!arr[i].equals(arr[i - 1])) bw.write(arr[i] + "\n");
+        }
         bw.close();
     }
 }
