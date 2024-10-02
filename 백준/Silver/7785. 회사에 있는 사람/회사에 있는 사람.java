4ef82fdfb193324dce16928345c768c410a2cf1e
suyeon <dltd0912@gmail.com>
2024-10-02 18:50:44 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 476 ms, Memory: 54824 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,26 +8,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        String[][] arr = new String[n][2];
+        Set<String> set = new TreeSet<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = st.nextToken();
-            arr[i][1] = st.nextToken();
-        }
+            String name = st.nextToken();
+            String statue = st.nextToken();
 
-        Arrays.sort(arr, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                return o2[0].compareTo(o1[0]);
-            }
-        });
-
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i][1].equals("enter") && !arr[i][0].equals(arr[i + 1][0])) {
-                bw.write(arr[i][0] + "\n");
-            }
+            if (statue.equals("enter")) set.add(name);
+            else set.remove(name);
         }
-        if (arr[n - 1][1].equals("enter")) bw.write(arr[n - 1][0]);
+        String[] answer = set.toArray(new String[set.size()]);
+        for (int i = answer.length - 1; i >= 0; i--) bw.write(answer[i] + "\n");
         bw.close();
+
     }
 }
