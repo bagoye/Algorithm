d918153d122212a194a43db48519b2e5101b8911
suyeon <dltd0912@gmail.com>
2024-10-07 15:55:24 +0900

    [Silver I] Title: Generic Queries, Time: 1152 ms, Memory: 304524 KB -BaekjoonHub

---
+++ 백준/Silver/16713. Generic Queries/Generic Queries.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        int[] nums = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            nums[i] = nums[i - 1] ^ arr[i];
+        }
+
+        int answer = 0;
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            answer ^= nums[e] ^ nums[s - 1];
+        }
+
+        bw.write(answer + "");
+        bw.close();
+    }
+}
