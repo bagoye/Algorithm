c4111fda2a2127b863cb9b778264a749775a392c
suyeon <dltd0912@gmail.com>
2024-10-08 17:15:28 +0900

    [level 1] Title: K번째수, Time: 0.43 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,17 +3,12 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int cnt = 0;
- 
-        for(int[] i : commands) {
-            ArrayList<Integer> arr = new ArrayList<>();
-            for(int j = i[0] - 1 ; j < i[1] ; j++) {
-                arr.add(array[j]);
-            }
-            Collections.sort(arr);
-            answer[cnt++] = arr.get(i[2] - 1);
+        
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
         }
- 
         return answer;
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 82.1 MB, 시간: 0.31 ms
+메모리: 66 MB, 시간: 0.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 18일 20:05:56
+2024년 10월 08일 17:15:25
 
 ### 문제 설명
 
