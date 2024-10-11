046855422fe58e680bc0b68ec04b40afb4995e4d
suyeon <dltd0912@gmail.com>
2024-10-11 22:13:17 +0900

    [Gold IV] Title: 부분합, Time: 840 ms, Memory: 22632 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        int min = Integer.MAX_VALUE;
+        boolean flag = false;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+
+                if (sum >= m) {
+                    min = Math.min(min, j - i + 1);
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+        if (!flag) bw.write("0");
+        else bw.write(min + "");
+        bw.close();
+    }
+}
