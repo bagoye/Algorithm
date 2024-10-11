4503eb7fc3ef9d5c57525f2b08acac137cf8f86d
suyeon <dltd0912@gmail.com>
2024-10-11 22:18:41 +0900

    [Gold IV] Title: 부분합, Time: 188 ms, Memory: 22532 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -10,28 +10,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
         int min = Integer.MAX_VALUE;
-        boolean flag = false;
+        int start = 0, end = 0, sum = 0;
 
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += arr[j];
-
-                if (sum >= m) {
-                    min = Math.min(min, j - i + 1);
-                    flag = true;
-                    break;
-                }
+        while (true) {
+            if (sum >= s) {
+                min = Math.min(min, end - start);
+                sum -= arr[start++];
+            } else if (end == n) {
+                break;
+            } else {
+                sum += arr[end++];
             }
         }
 
-        if (!flag) bw.write("0");
+        if (min == Integer.MAX_VALUE) bw.write("0");
         else bw.write(min + "");
+
         bw.close();
     }
 }
