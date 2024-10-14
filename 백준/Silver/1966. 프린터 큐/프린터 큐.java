7c033888d49281bea385350001e46e499c07346e
suyeon <dltd0912@gmail.com>
2024-10-14 21:21:16 +0900

    [Silver III] Title: 프린터 큐, Time: 84 ms, Memory: 12628 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,68 +1,47 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
+
+    static class Paper {
+        int index;
+        int priority;
+
+        Paper(int index, int priority) {
+            this.index = index;
+            this.priority = priority;
+        }
+
+    }
 
     public static void main(String[] args) throws IOException {
         int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
-            LinkedList<int[]> queue = new LinkedList<>();
             st = new StringTokenizer(br.readLine());
-
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
+            Queue<Paper> q = new LinkedList<>();
+            Integer[] prioritySeq = new Integer[n];
             st = new StringTokenizer(br.readLine());
-
             for (int i = 0; i < n; i++) {
-                // {초기 위치, 중요도}
-                queue.offer(new int[] { i, Integer.parseInt(st.nextToken()) });
+                prioritySeq[i] = Integer.parseInt(st.nextToken());
+                q.offer(new Paper(i, prioritySeq[i]));
             }
 
-            int cnt = 0; // 출력 횟수
-
-            while (!queue.isEmpty()) {
-                int[] front = queue.poll(); // 가장 첫 원소
-                boolean isMax = true; // front 원소가 가장 큰지 판
-
-                // 큐에 남아있는 원소들과 중요도 비교
-                for (int i = 0; i < queue.size(); i++) {
-
-                    // 처음 뽑은 원소보다 큐에 있는 i번째 원소가 중요도가 클 경우
-                    if (front[1] < queue.get(i)[1]) {
-
-                        // 뽑은 원소 및 i 이전의 원소들을 뒤로 보냄
-                        queue.offer(front);
-                        for (int j = 0; j < i; j++) {
-                            queue.offer(queue.poll());
-                        }
-
-                        // front 원소가 가장 큰 원소가 아니라 false를 하고 탐색 마침
-                        isMax = false;
-                        break;
-                    }
+            Arrays.sort(prioritySeq, Collections.reverseOrder());
+            for (int i = 0; i < n; i++) {
+                while (q.peek().priority != prioritySeq[i]) {
+                    q.offer(q.poll());
                 }
-
-                // front 원소가 가장 큰 원소가 아니라 다음 반복문으로 넘어감
-                if (isMax == false) {
-                    continue;
-                }
-
-                // front 원소가 가장 큰 원소였으므로 해당 원소는 출력해야 하는 문서
-                cnt++;
-                if (front[0] == m) {
-                    break;
-                }
+                if (q.peek().index == m) bw.write(i + 1 + "\n");
+                q.poll();
             }
-            sb.append(cnt).append("\n");
         }
-        bw.write(sb.toString());
         bw.close();
     }
 }
